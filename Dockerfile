d6652ea7c8335a77baf6494648614ec0c45e641d
Robert Bohne <robert.bohne@redhat.com>
2019-08-22 06:46:11 +0200

    Change member format from "master-0:192.168.222.31" to "master-0=192.168.222.31:80"

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM centos:7
 MAINTAINER Robert Bohne <robert.bohne@redhat.com>
 
 RUN yum install -y haproxy gettext && \
+    yum update -y && \
     yum clean all
 
 EXPOSE 6443
@@ -10,10 +11,10 @@ EXPOSE 80
 EXPOSE 443
 EXPOSE 22623
 
-ENV  API="bootstrap:192.168.222.30,master-0:192.168.222.31,master-1:192.168.222.32,master-3:192.168.222.33"
-ENV  INGRESS_HTTP="master-0:192.168.222.31,master-1:192.168.222.32,master-3:192.168.222.33,worker-0:192.168.222.34,worker-1:192.168.222.35,worker-3:192.168.222.36"
-ENV  INGRESS_HTTPS="master-0:192.168.222.31,master-1:192.168.222.32,master-3:192.168.222.33,worker-0:192.168.222.34,worker-1:192.168.222.35,worker-3:192.168.222.36"
-ENV  MACHEIN_CONFIG_SERVER="bootstrap:192.168.222.30,master-0:192.168.222.31,master-1:192.168.222.32,master-3:192.168.222.33"
+ENV  API="bootstrap=192.168.222.30:6443,master-0=192.168.222.31:6443,master-1=192.168.222.32:6443,master-3=192.168.222.33:6443"
+ENV  INGRESS_HTTP="master-0=192.168.222.31:80,master-1=192.168.222.32:80,master-3=192.168.222.33:80,worker-0=192.168.222.34:80,worker-1=192.168.222.35:80,worker-3=192.168.222.36:80"
+ENV  INGRESS_HTTPS="master-0=192.168.222.31:443,master-1=192.168.222.32:443,master-3=192.168.222.33:443,worker-0=192.168.222.34:443,worker-1=192.168.222.35:443,worker-3=192.168.222.36:443"
+ENV  MACHEIN_CONFIG_SERVER="bootstrap=192.168.222.30:22623,master-0=192.168.222.31:22623,master-1=192.168.222.32:22623,master-3=192.168.222.33:22623"
 
 # https://www.haproxy.org/download/1.8/doc/management.txt
 # "4. Stopping and restarting HAProxy"
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,30 +2,27 @@
 
 function build_conf {
     DATA=$1
-    PORT=$2
     IFS=,
     CONFIG=""
     for i in $DATA ; do 
-        # i containes 'NAME:IP'
-        #       ${i%:*} => NAME
-        #       ${i#*:} => IP
-        
-        CONFIG+="    server ${i%:*} ${i#*:}:$PORT check\n"
-
+        # i contains 'name=ip:port'        
+        #       ${i%:=} => name
+        #       ${i#*=} => ip:port
+        CONFIG+="    server ${i%=*} ${i#*=} check\n"
     done
     echo -e $CONFIG;
 }
 
 # Examples:
-#   API="bootstrap:192.168.222.30,master-0:192.168.222.31,master-1:192.168.222.32,master-3:192.168.222.33"
-#   INGRESS_HTTP="master-0:192.168.222.31,master-1:192.168.222.32,master-3:192.168.222.33,worker-0:192.168.222.34,worker-1:192.168.222.35,worker-3:192.168.222.36"
-#   INGRESS_HTTPS="master-0:192.168.222.31,master-1:192.168.222.32,master-3:192.168.222.33,worker-0:192.168.222.34,worker-1:192.168.222.35,worker-3:192.168.222.36"
-#   MACHEIN_CONFIG_SERVER="bootstrap:192.168.222.30,master-0:192.168.222.31,master-1:192.168.222.32,master-3:192.168.222.33"
+#   export API="bootstrap=192.168.222.30:6443,master-0=192.168.222.31:6443,master-1=192.168.222.32:6443,master-3=192.168.222.33:6443"
+#   export INGRESS_HTTP="master-0=192.168.222.31:80,master-1=192.168.222.32:80,master-3=192.168.222.33:80,worker-0=192.168.222.34:80,worker-1=192.168.222.35:80,worker-3=192.168.222.36:80"
+#   export INGRESS_HTTPS="master-0=192.168.222.31:443,master-1=192.168.222.32:443,master-3=192.168.222.33:443,worker-0=192.168.222.34:443,worker-1=192.168.222.35:443,worker-3=192.168.222.36:443"
+#   export MACHEIN_CONFIG_SERVER="bootstrap=192.168.222.30:22623,master-0=192.168.222.31:22623,master-1=192.168.222.32:22623,master-3=192.168.222.33:22623"
 # 
-export INGRESS_HTTP_CFG=$(build_conf $INGRESS_HTTP 80)
-export INGRESS_HTTPS_CFG=$(build_conf $INGRESS_HTTPS 443)
-export API_CFG=$(build_conf $API 6443)
-export MACHEIN_CONFIG_SERVER_CFG=$(build_conf $MACHEIN_CONFIG_SERVER 22623 )
+export INGRESS_HTTP_CFG=$(build_conf $INGRESS_HTTP)
+export INGRESS_HTTPS_CFG=$(build_conf $INGRESS_HTTPS)
+export API_CFG=$(build_conf $API)
+export MACHEIN_CONFIG_SERVER_CFG=$(build_conf $MACHEIN_CONFIG_SERVER)
 
 envsubst < haproxy-template.cfg > /haproxy.cfg
 
