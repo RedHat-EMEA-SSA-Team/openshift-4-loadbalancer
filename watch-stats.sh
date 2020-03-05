ca9f053ca9c7a5a387bd06243fdf2ca17ea61453
Gianni Salinetti <gbsalinetti@extraordy.com>
2020-03-05 21:20:24 +0100

    Replace watch command with builtin shell loop.

--- watch-stats.sh
+++ watch-stats.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-watch 'echo "show stat" | nc -U /var/lib/haproxy/stats | cut -d "," -f 1,2,18,57| column -s, -t'
+while true; do printf "\033c"; echo "show stat" | nc -U /var/lib/haproxy/stats | cut -d "," -f 1,2,18,57| column -s, -t; sleep 2; done
